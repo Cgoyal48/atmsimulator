048676c8e52c314a7aec46fa4c8a7da2d8fefcf7
Chetan Goyal <cgoyal2428@gmail.com>
2020-08-06 20:02:51 -0700

    First commit

---
+++ src/atmStimulator/conn.java
@@ -0,0 +1,17 @@
+package atmStimulator;
+
+import java.sql.*;
+
+public class conn {
+	Connection c;
+	Statement s;
+	public conn() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			c=DriverManager.getConnection("jdbc:mysql:///project","root","ACac2428");
+			s=c.createStatement();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
